7bf311db0678ce8a35bb34676581341117282aa6
0xGhazy <hhamdypro@gmail.com>
2024-07-10 02:23:10 -0700

    Update user moddel by adding more data attributes to user model #6

--- ServerApp/src/main/java/org/example/model/User.java
+++ ServerApp/src/main/java/org/example/model/User.java
@@ -2,28 +2,41 @@ package org.example.model;
 
 import lombok.Data;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 @Data
 public class User {
 
+    private Long id;
+    private String email;
+    private String firstName;
+    private String lastName;
     private String username;
     private String password;
     private String secQ;
     private String secA;
     private HashMap<String, Long> words = new HashMap<>();
     private ArrayList<String> conversations = new ArrayList<>();
+    private LocalDateTime createdAt = LocalDateTime.now();
+    private LocalDateTime updatedAt;
 
     @Override
     public String toString() {
         return "User{" +
-                "username='" + username + '\'' +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", username='" + username + '\'' +
                 ", password='" + password + '\'' +
                 ", secQ='" + secQ + '\'' +
                 ", secA='" + secA + '\'' +
                 ", words=" + words +
                 ", conversations=" + conversations +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt +
                 '}';
     }
 }
