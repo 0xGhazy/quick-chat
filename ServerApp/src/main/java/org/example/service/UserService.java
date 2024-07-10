6560c528660f034abba76dbc073813c91d07d65c
0xGhazy <hhamdypro@gmail.com>
2024-07-10 01:54:09 -0700

    Adding user service implementation

--- ServerApp/src/main/java/org/example/service/UserService.java
+++ ServerApp/src/main/java/org/example/service/UserService.java
@@ -2,19 +2,83 @@ package org.example.service;
 
 import com.google.gson.Gson;
 import org.example.model.User;
+import org.example.repository.UserRepository;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Optional;
 
 
 public class UserService {
 
-    private Gson gson = new Gson();
+    private final Gson gson = new Gson();
+    private final UserRepository repository = new UserRepository();
 
-    public String serialize(User user) {
+
+    public Optional<User> insert(User user) {
+        try {
+            return repository.insertUser(user);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Optional<User> update(Long id, User user) {
+        try {
+            return repository.updateUserById(id, user);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Optional<User> loadUserByUsernameAndPassword(String username, String password) {
+        try {
+            return repository.findByUsernameAndPassword(username, password);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Optional<User> findById(Long id) {
+        try {
+            return repository.findById(id);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Optional<User> findById(String email) {
+        try {
+            return repository.findByEmail(email);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String jsonSerialize(User user) {
         return gson.toJson(user);
     }
 
-    public User deSerialize(String userJson) {
+    public User jsonToObject(String userJson) {
         return gson.fromJson(userJson, User.class);
     }
-
 
 }
